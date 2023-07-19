b78243ed48b244f454327bfff6476a3f01a150fd
niteshrawat0401 <niteshrawat0401@gmail.com>
2023-07-19 20:58:33 +0530

    cors

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const bmiRouter = require('./router/bmiRouter');
 require("dotenv").config();
 
 const app = express()
-app.use(cors({origin : ["http://localhost:3000"]}))
-
+// app.use(cors({origin : ["*"]}))
+app.use(cors())
 app.use(express.urlencoded({extended: true}))
 app.use(express.json());
 
